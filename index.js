0eac37b392f1db858d8d3d35677b8c2172070bdb
Edilene Da Cruz <hello@edilene.io>
2017-10-28 22:28:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function total() {
   let i = 0
 
   while(i < cart.length) {
-    totalCost += parseInt(Object.keys(cart[i]))
+    totalCost += Object.keys(cart[i])[0])
   }
   return totalCost
 }
